66dc2418bb5419640c78005fb87d0e7b41368a87
FedericoAureliano <fmorarocha@gmail.com>
2017-12-04 12:40:49 -0500

    cleaning titles

--- plot.sh
+++ plot.sh
@@ -11,12 +11,12 @@ do
     if [ -d $f ] ; then
         echo $f;
         cd $f;
-        /home/fmora/workspace/StringSMTBenchmarks/bin/json2cactus.py "$f" 15 `find . -name '*.json'` > cactus.png
-        /home/fmora/workspace/StringSMTBenchmarks/bin/json2compare.py "$f" `find . -name '*.json'` > comparison.png
+        $dir/bin/json2cactus.py ${f#"problems/"} 15 `find . -name '*.json'` > cactus.png
+        $dir/bin/json2compare.py ${f#"problems/"} `find . -name '*.json'` > comparison.png
         cd $dir
     fi
 done
 
 cd problems
-/home/fmora/workspace/StringSMTBenchmarks/bin/json2cactus.py "$DATE" 15 `find . -name '*.json'` > cactus.png
-/home/fmora/workspace/StringSMTBenchmarks/bin/json2compare.py "$DATE" `find . -name '*.json'` > comparison.png
+$dir/bin/json2cactus.py "$DATE" 15 `find . -name '*.json'` > cactus.png
+$dir/bin/json2compare.py "$DATE" `find . -name '*.json'` > comparison.png
